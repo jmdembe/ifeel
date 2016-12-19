8035091e90b0739bc12a7a9fcb6629010fd878a3
Jessica Dembe <jmdembe@gmail.com>
2016-12-19 14:24:02 -0500

    Added watch, clean, and copy to grunt tasks

--- gruntfile.js
+++ gruntfile.js
@@ -3,14 +3,41 @@
 module.exports = function(grunt) {
 
   grunt.initConfig ({
+    clean: ['build/'],
+
+    copy: {
+      html: {
+        files: [
+          {
+            expand: true,
+            cwd: 'src/',
+            dest: 'build'
+          }
+        ]
+      }
+    },
+    watch: {
+      html: {
+        files: ['src/index.html'],
+        tasks: ['copy:html']
+      },
+      sass: {
+        files: ['src/sass/**/*.scss'],
+        tasks: ['sass']
+      }
+    },
     sass: {
       styles: {
         files: {
           'build/css/style.css': 'src/sass/main.scss'
         }
       }
-    }
+    },
+
   });
 
-grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 };
