b4a93fb03a8ac306522010a03bb8c5ea7735aec2
Jessica Dembe <jmdembe@gmail.com>
2016-12-20 13:30:23 -0500

    Added jshint to package.json, added ui router to index, created module file, and added jshint config file

--- src/js/ifeel.module.js
+++ src/js/ifeel.module.js
@@ -1,4 +1,24 @@
 (function() {
   'use strict';
-  
+
+  angular.module('ifeel', ['ui.router'])
+    .config(routerConfig);
+
+  routerConfig.$inject = ['$stateProvider'];
+
+  function routerConfig($stateProvider){
+    $stateProvider
+
+      .state({
+        name: 'home',
+        url: '',
+        templateUrl: 'views/login.template.html'
+      })
+
+      .state({
+        name: 'dashboard',
+        url: '/dashboard',
+        templateUrl: 'input.template.html'
+      })
+  }
 }());
