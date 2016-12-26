49f76a97b28c7534b3721e4ae14a14907ca827d6
Jessica Dembe <jmdembe@gmail.com>
2016-12-26 15:23:26 -0500

    Re-named service file, added to login template

---
+++ src/client/js/fitbit.service.js
@@ -0,0 +1,25 @@
+(function() {
+  'use strict';
+
+  angular.module('ifeel')
+    .factory('FitBitService', FitBitService);
+
+    FitBitService.inject = ['$http'];
+
+    function FitBitService($http) {
+      return {
+        getActivity: getActivity,
+      };
+
+      function getActivity() {
+        console.log("this is activity data");
+        return $http({
+          url: 'https://api.fitbit.com/1/user/:user-id/activities/date/:date.json',
+          method: 'GET'
+        })
+        .then(function successHandler(response) {
+          return response.data;
+        });
+      }
+    }
+}());
