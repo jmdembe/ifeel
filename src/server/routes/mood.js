a52e0de6aac733946821f36932f5396951dc1850
Jessica Dembe <jmdembe@gmail.com>
2016-12-29 20:28:11 -0500

    Added title to index, removed unnecesary line in mood route, added server to jshint

--- src/server/routes/mood.js
+++ src/server/routes/mood.js
@@ -2,7 +2,6 @@
 
   var express = require('express');
   var moodModel = require('../models/mood');
-
   var router = express.Router();
 
   router.get('/', function getAll(req, res) {
@@ -10,17 +9,6 @@
       if (err) {
         console.error(err);
         res.status(500).send("I dont know what is wrong with the server...");
-        return;
-      }
-      res.json(data);
-    });
-  });
-
-  router.get(':/id', function getToday(req, res) {
-    moodModel.getOne(req.params.id, dataRetrieved(err, data) {
-      if(err) {
-        console.error(err);
-        res.status(500).send('Failed to retrieve your job');
         return;
       }
       res.json(data);
