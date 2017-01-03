d455f4f38af3e18b6922edc1433638ca7db1e1d9
Jessica Dembe <jmdembe@gmail.com>
2017-01-02 19:50:06 -0500

    Created dock blocks for functions

--- src/client/js/login.service.js
+++ src/client/js/login.service.js
@@ -15,11 +15,23 @@
       retrieveProfileInfo: retrieveProfileInfo
     };
 
+    /**
+     * This function stores user profile information after the user has logged into the app.
+     * Login information is then stored into profile.
+     * @param  {Object} user [description]
+     * @return {VOID}      [description]
+     */
     function storeBasicProfile(user) {
+      console.log(user);
       profile = user.getBasicProfile();
       console.log('profile info', profile);
     }
 
+    /**
+     * This function retrieves user profile information once called. The function returns
+     * an array with profile information.
+     * @return {[type]} [description]
+     */
     function retrieveProfileInfo() {
       return profile;
     }
