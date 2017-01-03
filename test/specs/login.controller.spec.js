a904c7ffda62566eea6cf2a70474ff6fb72fcfcf
Jessica Dembe <jmdembe@gmail.com>
2017-01-03 17:37:19 -0500

    Created spec file for login controller

---
+++ test/specs/login.controller.spec.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+
+  describe('Login Controller', function () {
+    var LoginController;
+
+    beforeEach(module('ifeel'));
+
+    beforeEach(inject(function($controller) {
+      LoginController = $controller('LoginController');
+    }));
+
+
+  });
+}());
