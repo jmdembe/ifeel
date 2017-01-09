d02066cb6cd4b1cf8dff89e9bca50d995c2c678e
Jessica Dembe <jmdembe@gmail.com>
2017-01-09 18:23:42 -0500

    Added tests to mood service spec

--- test/specs/mood.service.spec.js
+++ test/specs/mood.service.spec.js
@@ -16,7 +16,7 @@
       $rootScope = _$rootScope_;
 
       $httpBackend
-        .whenGET('/mood')
+        .whenPOST('/mood')
         .respond({
           "_id": "586fa5aa3b5c861bf8ddb5b9",
           "profile": {
@@ -32,16 +32,68 @@
           "createTime": 1483711914160
         });
 
+        $httpBackend
+          .whenGET('/mood')
+          .respond({
+            "_id": "586fa5aa3b5c861bf8ddb5b9",
+            "profile": {
+              "Eea": "110514567623357669787",
+              "ig": "Jessica Dembe",
+              "ofa": "Jessica",
+              "wea": "Dembe",
+              "Paa": "https://lh4.googleusercontent.com/-MHhjrb1-pxo/AAAAAAAAAAI/AAAAAAAAAaU/ZOB30-gKm1g/s96-c/photo.jpg",
+              "U3": "jmdembe@gmail.com"
+            },
+            "mood": "great",
+            "postDate": 1483711914100,
+            "createTime": 1483711914160
+          });
+
       $httpBackend
         .whenGET('views/home.template.html')
         .respond('At the home template');
+
+        $httpBackend
+          .whenGET('views/input.template.html')
+          .respond('At the input template');
+
+        $httpBackend
+          .whenGET('views/confirm.template.html')
+          .respond('At the confirm template');
     }));
 
-    it('Should be a function to save to the database', function() {
-      var result = MoodService.createToday({"mood":"great", 'postDate': 1483711914100});
+    it('Should post information to the database', function(dc) {
+      var result = MoodService.createToday({'mood':'great', 'postDate': 1483711914100});
       expect(result).to.be.an('object');
       expect(result.then).to.be.a('function');
       expect(result.catch).to.be.a('function');
+
+      result
+        .then(function(data) {
+          expect(data).to.be.an('object');
+          expect(data.mood).to.be.a('string');
+          expect(data.mood).to.equal('great');
+          expect(data.postDate).to.be.a('number');
+          expect(data.postDate).to.equal(1483711914100);
+          dc();
+        })
+        .catch(function(xhr) {
+          console.log(xhr.message);
+          dc('Why is this happening?', xhr.message);
+        });
+        $httpBackend.flush();
+    });
+
+    it('Should get information from the database', function() {
+      var result = MoodService.getAll();
+      expect(result).to.be.an('object');
+      expect(result.then).to.be.a('function');
+      expect(result.catch).to.be.a('function');
+
+      result
+        .then(function(data) {
+          expect(data).to.be.an('object');
+        });
     });
   });
-}());
+})();
