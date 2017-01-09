16ababd01ec556a7bb0c7e722b9392652f9c2407
Jessica Dembe <jmdembe@gmail.com>
2017-01-09 11:19:56 -0500

    removed chart.js dependency from linectrl controller

--- src/client/js/linectrl.controller.js
+++ src/client/js/linectrl.controller.js
@@ -1,13 +1,14 @@
 (function() {
   'use strict';
 
-  angular.module('ifeel', ['chart.js'])
+  angular.module('ifeel')
     .controller('LineCtrl', LineCtrl);
 
-    LineCtrl.$inject = ['MoodService'];
+    LineCtrl.$inject = ['$state', 'MoodService'];
 
-    function LineCtrl(MoodService) {
+    function LineCtrl($state, MoodService) {
       // TODO: Get all the dates from server.
+      console.log(arguments);
       this.labels = MoodService.getAll().postDate;
       this.series = ['Mood', 'Description'];
       this.onClick = function(points,evt) {
