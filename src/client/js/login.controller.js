e411960592089005710e9899dd6e705c67688c65
Jessica Dembe <jmdembe@gmail.com>
2017-01-09 11:20:36 -0500

    Removed extraneous line break in login controller

--- src/client/js/login.controller.js
+++ src/client/js/login.controller.js
@@ -8,9 +8,6 @@
 
     function LoginController($state, LoginService) {
 
-      console.log('login controller created');
-      this.bar = 'baz';
-
       this.onSignIn = function onSignIn(googleUser){
         console.log('logged into google');
         LoginService.storeBasicProfile(googleUser);
