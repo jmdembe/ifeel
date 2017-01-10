1a9e9fa095a65fa5aaad441a66738bb1c8311876
Jessica Dembe <jmdembe@gmail.com>
2017-01-10 13:52:17 -0500

    Re-added chart controller with scope to have a functional chart, changed alias of scope to match new matching scope

--- src/client/js/chart.controller.js
+++ src/client/js/chart.controller.js
@@ -4,16 +4,44 @@
   angular.module('ifeel')
     .controller('ChartController', ChartController);
 
-    ChartController.$inject = ['$state','MoodService'];
+    ChartController.$inject = ['MoodService'];
 
-    function ChartController($state, MoodService) {
-      console.log('creating chart controller');
-      // TODO: Get all the dates from server.
-      console.log(arguments);
-      this.labels = MoodService.getAll().postDate;
-      this.series = ['Mood', 'Description'];
-      this.onClick = function(points,evt) {
+    function ChartController(MoodService) {
+      this.moodRecordsData = null;
+      var vm = this;
+
+      MoodService.getAll()
+        .then(function addDataToScope(data){
+          vm.moodRecordsData = data;
+        });
+
+      this.labels = ['Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+      this.series = ['Series A', 'Series B'];
+      this.data = [
+        [65, 59, 80, 81, 56, 55, 40],
+        [28, 48, 40, 19, 86, 27, 90]
+      ];
+      this.onClick = function (points, evt) {
         console.log(points, evt);
+      };
+      this.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+      this.options = {
+        scales: {
+          yAxes: [
+            {
+              id: 'y-axis-1',
+              type: 'linear',
+              display: true,
+              position: 'left'
+            },
+            {
+              id: 'y-axis-2',
+              type: 'linear',
+              display: true,
+              position: 'right'
+            }
+          ]
+        }
       };
     }
 }());
