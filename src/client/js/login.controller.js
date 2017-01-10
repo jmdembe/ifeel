d21a136061b3731219252811934c9260cb438986
Jessica Dembe <jmdembe@gmail.com>
2017-01-09 19:00:44 -0500

    Removed extraneous lines from login controller

--- src/client/js/login.controller.js
+++ src/client/js/login.controller.js
@@ -9,7 +9,6 @@
     function LoginController($state, LoginService) {
 
       this.onSignIn = function onSignIn(googleUser){
-        console.log('logged into google');
         LoginService.storeBasicProfile(googleUser);
         $state.go('dashboard');
       };
